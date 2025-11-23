d1550cf6220613e2aede54a88399e0843d8c1777
eldespertarpr <eldespertarpr@gmail.com>
2025-11-23 16:59:57 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
-const CACHE='siembra-v10';
-const FILES=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png',
-'./img/aji.jpg','./img/pimiento.jpg','./img/cilantrillo.jpg','./img/recao.jpg','./img/tomate.jpg',
-'./img/lechuga.jpg','./img/cebolla.jpg','./img/menta.jpg','./img/zanahoria.jpg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE = 'siembra-v13';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './img/aji.jpg',
+  './img/pimiento.jpg',
+  './img/cilantro.jpg',
+  './img/recao.jpg',
+  './img/tomate.jpg',
+  './img/lechuga.jpg',
+  './img/cebolla.jpg',
+  './img/menta.jpg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
