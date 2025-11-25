a58f8c5a33757a23081b7f059e8a45930bb19690
eldespertarpr <eldespertarpr@gmail.com>
2025-11-25 09:48:15 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,39 @@
-const CACHE = 'siembra-v14';
-const FILES = [
+const CACHE_NAME = 'mi-siembra-v14';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './sw.js',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-1024.png'
+  // Si quieres, luego añadimos aquí las imágenes de los cultivos
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
+self.addEventListener('activate', event => {
+  event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
