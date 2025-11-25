f5b4c714f36d6157578dcf982f8e5509a43838da
eldespertarpr <eldespertarpr@gmail.com>
2025-11-24 22:17:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = 'mi-siembra-v14';
+const CACHE_NAME = 'mi-siembra-v13';
+
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-72.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './img/icon/icon-192.png',
+  './img/icon/icon-512.png',
+  './img/icon/icon-1024.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
@@ -20,24 +20,18 @@ self.addEventListener('activate', event => {
     caches.keys().then(keys =>
       Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
       )
     )
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  const req = event.request;
-
-  // Solo GET
-  if (req.method !== 'GET') return;
-
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(req).then(cached => {
-      if (cached) return cached;
-      return fetch(req).catch(() => caches.match('./index.html'));
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
